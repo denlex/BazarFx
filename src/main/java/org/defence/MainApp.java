47db34c799d5b76db2b383fb7274b420e94d6584
ndaleksey <ugttm>
2015-07-22 14:41:16 +0300

    Создана минимальная структура проекта

---
+++ src/main/java/org/defence/MainApp.java
@@ -0,0 +1,26 @@
+package org.defence;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by root on 22.07.15.
+ */
+public class MainApp extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        String fxmlFile = "/fxml/hello.fxml";
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
+        stage.setTitle("JavaFX and Maven");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
