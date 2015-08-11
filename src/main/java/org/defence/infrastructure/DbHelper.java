8a13afbc8999d2c34bf42f6a468f93044eaca743
denlex <denlex@gmail.com>
2015-08-11 14:48:41 +0300

    Добавил процедуру импорта единиц измерений и типов единиц иземерений (рабочая версия)

--- src/main/java/org/defence/infrastructure/DbHelper.java
+++ src/main/java/org/defence/infrastructure/DbHelper.java
@@ -1,5 +1,7 @@
 package org.defence.infrastructure;
 
+import org.defence.domain.entities.Characteristic;
+import org.defence.domain.entities.CharacteristicType;
 import org.defence.domain.entities.Measurement;
 import org.defence.domain.entities.MeasurementType;
 import org.hibernate.Session;
@@ -10,17 +12,16 @@ import org.hibernate.cfg.Configuration;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by root on 8/10/15.
  */
-class MeasurementEntry {
+abstract class Entry {
     private int groupId;
-    private Measurement measurement;
 
-    public MeasurementEntry(int groupId, Measurement measurement) {
+    public Entry(int groupId) {
         this.groupId = groupId;
-        this.measurement = measurement;
     }
 
     public int getGroupId() {
@@ -29,6 +30,15 @@ class MeasurementEntry {
 
     public void setGroupId(int groupId) {
         this.groupId = groupId;
+    }
+}
+
+class MeasurementEntry extends Entry {
+    private Measurement measurement;
+
+    public MeasurementEntry(int groupId, Measurement measurement) {
+        super(groupId);
+        this.measurement = measurement;
     }
 
     public Measurement getMeasurement() {
@@ -40,6 +50,23 @@ class MeasurementEntry {
     }
 }
 
+class CharacteristicEntry extends Entry {
+    private Characteristic characteristic;
+
+    public CharacteristicEntry(int groupId, Characteristic characteristic) {
+        super(groupId);
+        this.characteristic = characteristic;
+    }
+
+    public Characteristic getCharacteristic() {
+        return characteristic;
+    }
+
+    public void setCharacteristic(Characteristic characteristic) {
+        this.characteristic = characteristic;
+    }
+}
+
 public class DbHelper {
     private static SessionFactory factory;
     private static final String CHARACTERISTICS = "/home/contragent/Desktop/Characteristics.csv";
@@ -47,8 +74,15 @@ public class DbHelper {
     private static final String GROUPS_MEASUREMENTS = "/home/contragent/Desktop/GroupsMeasurements.csv";
     private static final String MEASUREMENTS = "/home/contragent/Desktop/Measurements.csv";
     private static List<MeasurementEntry> measurementEntries = new ArrayList<MeasurementEntry>();
-    private static Set<Integer> measurementGroupsId = new HashSet<Integer>();
     private static Map<Integer, MeasurementType> measurementTypes = new HashMap<Integer, MeasurementType>();
+    private static List<CharacteristicEntry> characteristicEntries = new ArrayList<CharacteristicEntry>();
+    private static Map<Integer, CharacteristicType> characteristicTypes = new HashMap<Integer, CharacteristicType>();
+
+    public static int importEntityIntoTable() throws Exception {
+        int counter = 0;
+
+        return counter;
+    }
 
     public static int importMeasurementsIntoTable() throws Exception {
         int counter = 0;
@@ -56,21 +90,13 @@ public class DbHelper {
         String line;
 
         while ((line = reader.readLine()) != null) {
-            if (parseMeasurementFormLine(line)) {
-                counter++;
-            }
+            parseMeasurementFormLine(line);
         }
-
-        System.out.println("Measurement file was parsed!");
 
         reader = new BufferedReader(new FileReader(GROUPS_MEASUREMENTS));
         while ((line = reader.readLine()) != null) {
-            if (parseMeasurementTypeFromLine(line)) {
-
-            }
+            parseMeasurementTypeFromLine(line);
         }
-
-        System.out.println("MeasurementType file was parsed!");
 
         importMeasurementTypes();
 
@@ -122,40 +148,147 @@ public class DbHelper {
 
     private static void importMeasurementTypes() throws ExceptionInInitializerError {
         if (measurementEntries.size() > 0) {
-            factory = new Configuration().configure().buildSessionFactory();
-            Session session = factory.openSession();
-            Transaction transaction = session.beginTransaction();
             Set<Measurement> measurements = new HashSet<Measurement>();
 
             // loading measurementTypes into MeasurementType table
-            for (Map.Entry<Integer, MeasurementType> entry : measurementTypes.entrySet()) {
+            for (final Map.Entry<Integer, MeasurementType> entry : measurementTypes.entrySet()) {
+                List<MeasurementEntry> list = measurementEntries.stream().filter(
+                        p -> p.getGroupId() == entry.getKey()).collect(Collectors.toList());
 
-                for (MeasurementEntry measurementEntry : measurementEntries) {
-                    if (entry.getKey() == measurementEntry.getGroupId()) {
-                        measurements.add(measurementEntry.getMeasurement());
-                    }
+                System.out.println(entry.getValue().getName());
+
+                if (list.size() <= 0) {
+                    break;
                 }
 
-                if (measurements.size() > 0) {
-                    entry.getValue().setMeasurements(measurements);
-                    session.save(entry.getValue());
-                    measurements.clear();
+                for (MeasurementEntry measurement : list) {
+                    measurements.add(measurement.getMeasurement());
+                    System.out.println("\t" + measurement.getMeasurement().getName() + "; " + measurement.getMeasurement().getShortName());
                 }
-            }
 
-            // loading measurements into Measurement table
-            for (MeasurementEntry measurementEntry : measurementEntries) {
-                session.save(measurementEntry.getMeasurement());
+                entry.getValue().setMeasurements(measurements);
+                loadMeasurementType(entry);
+                measurements.clear();
             }
 
             System.out.println("\nMEASUREMENTS were loaded\n");
-
-            transaction.commit();
         }
     }
 
+    private static void loadMeasurementType(Map.Entry<Integer, MeasurementType> entry) {
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(entry.getValue());
+        transaction.commit();
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+    /*public static int importCharacteristicsIntoTable() throws Exception {
+        int counter = 0;
+        BufferedReader reader = new BufferedReader(new FileReader(CHARACTERISTICS));
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            parseCharacteristicFormLine(line);
+        }
+
+        reader = new BufferedReader(new FileReader(GROUPS_CHARACTERISTICS));
+        while ((line = reader.readLine()) != null) {
+            parseCharacteristicTypeFromLine(line);
+        }
+
+        importCharacteristicTypes();
+
+        return counter;
+    }
+
+    private static boolean parseCharacteristicFormLine(String line) {
+        String[] words = line.split(";");
+
+        if (words.length != 2) {
+            System.out.println("Error in line: " + line);
+            return false;
+        }
+
+        String id = words[0];
+        String name = words[1];
+
+        try {
+            characteristicTypes.put(Integer.valueOf(id), new CharacteristicType("code", name));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+
+    private static boolean parseCharacteristicTypeFromLine(String line) {
+        String[] words = line.split(";");
+
+        if (words.length != 4) {
+            System.out.println("Error in line: " + line);
+            return false;
+        }
+
+        String name = words[1];
+        String shortName = words[2];
+        String groupNumber = words[3];
+
+        Characteristic characteristic = new Characteristic("", name);
+
+        try {
+            characteristicEntries.add(new CharacteristicEntry(Integer.valueOf(groupNumber), characteristic));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }*/
+
+    /*private static void importCharacteristicTypes() {
+        if (measurementEntries.size() > 0) {
+            Set<Characteristic> characteristics = new HashSet<Characteristic>();
+
+            // loading measurementTypes into CharacteristicType table
+            for (final Map.Entry<Integer, CharacteristicType> entry : characteristicTypes.entrySet()) {
+                List<CharacteristicEntry> list = characteristicEntries.stream().filter(
+                        p -> p.getGroupId() == entry.getKey()).collect(Collectors.toList());
+
+                System.out.println(entry.getValue().getName());
+
+                if (list.size() <= 0) {
+                    break;
+                }
+
+                for (CharacteristicEntry characteristic : list) {
+                    characteristics.add(characteristic.getMeasurement());
+                    System.out.println("\t" + characteristic.getMeasurement().getName() + "; " + characteristic.getMeasurement().getShortName());
+                }
+
+                entry.getValue().setCharacteristics(characteristics);
+                loadCharacteristicType(entry);
+                characteristics.clear();
+            }
+
+            System.out.println("\nMEASUREMENTS were loaded\n");
+        }
+    }*/
+
+    private static void loadCharacteristicType(Map.Entry<Integer, CharacteristicType> entry) {
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(entry.getValue());
+        transaction.commit();
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+
     public static void main(String[] args) throws Exception {
+        factory = new Configuration().configure().buildSessionFactory();
+
         int result = importMeasurementsIntoTable();
         System.out.format("%s measurements were loaded\n", result);
+
+        factory.close();
     }
 }
