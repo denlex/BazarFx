f2d89f2e1ebf750c266b43a70728f81b6eaf3bc7
denlex <denlex@gmail.com>
2015-08-14 16:22:07 +0300

    Созданы сущности для проверки Properties (незавершено)

--- src/main/java/org/defence/tests/PropertyTest.java
+++ src/main/java/org/defence/tests/PropertyTest.java
@@ -1,7 +1,33 @@
 package org.defence.tests;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Created by root on 8/14/15.
  */
-public class PropertyTest {
+public class PropertyTest extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        String fxmlFile = "/fxml/PropertiesForm.fxml";
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = loader.load(getClass().getResourceAsStream(fxmlFile));
+        stage.setTitle("JavaFX and Maven");
+
+        final Scene scene = new Scene(root);
+
+        scene.setFill(null);
+        stage.setScene(scene);
+        stage.setMinHeight(600);
+        stage.setMinWidth(1000);
+        stage.setResizable(true);
+        stage.show();
+    }
 }
